63d5f464c5fb4d3183639c5c9cf55eceae02853a
David Avikasis <deddy@deddyxyz16.local>
2022-12-27 05:06:02 +0200

    og_url and debug

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -22,5 +22,9 @@ export const DEFAULT_META_DATA = {
     "An Ultra-Fullstack Developer with over 10 years of experience working in the web development industry, handling everything from Infrastructure architecture & DevOps, Backendscripts and APIs, to Frontend - UX, Design and Development. Building web apps and systems from scratch to the user screen. This is my CV, a collection of my work and experience.",
   og_description:
     "An Ultra-Fullstack Developer with over 10 years of experience building web apps from scratch. This is my CV, a collection of my work and experience.",
+  og_url: process.env.URL,
   robots: "index, follow",
 };
+
+console.log("DEDDY: DEFAULT_META_DATA", DEFAULT_META_DATA);
+console.log("DEDDY: CONTENTFUL", process.env.CONTENTFUL_SPACE_ID);
